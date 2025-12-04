cc0c86790b0d19a5ce422ad613c8dcb494042003
Chinmay S B <cbh.mavim@gmail.com>
2025-12-04 21:45:34 +0530

    Created User's

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from db import models
+from db.database import engine
+from router import user
 
 app = FastAPI(
     title="Instagram API"
 )
+app.include_router(user.router)
+
+
+
+@app.get("/")
+def root():
+    return {
+        "message" : "Hello World!!"
+    }
+
+models.Base.metadata.create_all(engine)
